d7202b0b3df0ac9f7c7a55e49724ed71244275db
Tarick <gth766q@gatech.edu>
2024-08-29 15:55:49 -0400

    populate img tags directly in AsyncImage; add support for css modules

---
+++ client/css.d.ts
@@ -0,0 +1,2 @@
+// NOTE: Not sure why this is necessary if react-scripts 2+ supports css modules
+declare module "*.module.css";
--- client/src/components/AsyncImage/index.tsx
+++ client/src/components/AsyncImage/index.tsx
@@ -1,16 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+import styles from "./styles.module.css";
 
 type Props = {
   url: string;
 };
 
 const AsyncImage: React.FC<Props> = (props: Props) => {
-  const [imageData, setImageData] = useState<ImageData | null>(null);
+  /* const [imageData, setImageData] = useState<ImageData | null>(null); */
   const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   const { url } = props;
 
+  const onLoad = useCallback(() => {
+    setIsLoading(false);
+    setHasError(false);
+  }, []);
+
+  const onError = useCallback(() => {
+    setIsLoading(false);
+    setHasError(true);
+  }, []);
+
   useEffect(() => {
+    setIsLoading(true);
+    setHasError(false);
+  }, [url]);
+
+  // NOTE: Can alternatively use below routine depending on host cross-origin policy
+  /*
+	useEffect(() => {
     const fetchImage = async () => {
       if (url) {
         try {
@@ -29,16 +48,19 @@ const AsyncImage: React.FC<Props> = (props: Props) => {
     setIsLoading(true);
     fetchImage();
   }, [url]);
+	*/
 
   return (
-    <div>
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : imageData ? (
-        <img src={url} alt="Fetched Image" />
-      ) : (
-        <p>No image available.</p>
-      )}
+    <div
+      className={`${isLoading ? styles.loading : ""} ${
+        hasError ? styles.error : ""
+      }`}
+    >
+      <img onLoad={onLoad} onError={onError} src={url} alt="Fetched Image" />
+
+      {/* overlays */}
+      <p>Loading...</p>
+      <p>No image available.</p>
     </div>
   );
 };
