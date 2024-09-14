0b6ae4de05dce99649e238bf21c6ba6aa8083087
Tarick <gth766q@gatech.edu>
2024-09-13 20:30:08 -0400

    [WIP] KnowledgeBlock action & result groups with submission & validation

--- client/src/components/KnowledgeBlock/index.tsx
+++ client/src/components/KnowledgeBlock/index.tsx
@@ -9,14 +9,29 @@ type Props = {
   knowledgeCheckBlock: AggregatedKnowledgeBlock;
 };
 
+type Status = "correct" | "incorrect" | "error" | "no-answer" | "default";
+
 const KnowledgeBlock: React.FC<Props> = ({ knowledgeCheckBlock }: Props) => {
   const [selectedAnswerId, setSelectedAnswerId] = useState<string | null>(null);
   const [prevAnswerId, setPrevAnswerId] = useState<string | null>(null);
   const [uiStateId, setUiStateId] = useState<string | null>(null);
+  const [status, setStatus] = useState<Status>("default");
+
+  const handleOnChangeAnswer = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSelectedAnswerId(e.target.value);
+      setStatus("default");
+    },
+
+    [selectedAnswerId]
+  );
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
-    console.log(selectedAnswerId);
+
+    if (!selectedAnswerId) {
+      setStatus("no-answer");
+    }
   };
 
   // Initialize selected answers, if available
@@ -108,7 +123,7 @@ const KnowledgeBlock: React.FC<Props> = ({ knowledgeCheckBlock }: Props) => {
                       type={"radio"}
                       value={kba.answerId}
                       name={kba.answerId}
-                      onChange={(e) => setSelectedAnswerId(e.target.value)}
+                      onChange={handleOnChangeAnswer}
                       checked={selectedAnswerId === kba.answerId}
                     />
                     <label>{kba.answerText}</label>
@@ -116,7 +131,45 @@ const KnowledgeBlock: React.FC<Props> = ({ knowledgeCheckBlock }: Props) => {
                 ))}
             </ul>
           </div>
-          <button type="submit">Submit</button>
+          <div className={styles["knowledge-block__action-group"]}>
+            {status === "no-answer" ? (
+              <div
+                className={styles["knowledge-block__action-group__validation"]}
+              >
+                <span style={{ marginInlineEnd: "1rem" }}>
+                  <svg
+                    aria-hidden="true"
+                    focusable="false"
+                    data-prefix="far"
+                    data-icon="circle-exclamation"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 512 512"
+                    style={{
+                      display: "inline-block",
+                      height: "1em",
+                      overflow: "visible",
+                      verticalAlign: "-0.125em",
+                    }}
+                  >
+                    <path
+                      fill="currentColor"
+                      d="M256 0C114.6 0 0 114.6 0 256s114.6 256 256 256s256-114.6 256-256S397.4 0 256 0zM256 464c-114.7 0-208-93.31-208-208S141.3 48 256 48s208 93.31 208 208S370.7 464 256 464zM256 304c13.25 0 24-10.75 24-24v-128C280 138.8 269.3 128 256 128S232 138.8 232 152v128C232 293.3 242.8 304 256 304zM256 337.1c-17.36 0-31.44 14.08-31.44 31.44C224.6 385.9 238.6 400 256 400s31.44-14.08 31.44-31.44C287.4 351.2 273.4 337.1 256 337.1z"
+                    ></path>
+                  </svg>
+                </span>
+                Please answer the question to continue
+              </div>
+            ) : null}
+            {["default", "no-answer"].includes(status) ? (
+              <button type="submit">Submit</button>
+            ) : null}
+          </div>
+          <div className={styles["knowledge-block__result-group"]}>
+            <div className={styles[`knowledge-block__result-group__icon`]}>
+              <i className={styles[`${status}`]}></i>
+            </div>
+          </div>
         </form>
       </div>
     </section>
